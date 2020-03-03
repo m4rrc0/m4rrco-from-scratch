87c6c3078e8692b6a178574670a9c5cc37bc7ca5
MarcCoet <marcoet@gmail.com>
2020-03-03 22:43:07 +0100

    WIP: custom router

---
+++ src/Route.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { onDestroy } from "svelte";
+  import { curRoute } from "./store/index";
+  import router from "./router";
+  let component;
+  const unsubscribe = curRoute.subscribe(value => {
+    component = router.filter(r => r.path === $curRoute)[0].component;
+  });
+  onDestroy(unsubscribe);
+</script>
+
+<div class="main-container">
+  <svelte:component this={component} />
+</div>
