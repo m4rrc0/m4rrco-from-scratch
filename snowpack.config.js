25601825d23f15941fc5215ff487084ef75f2bc6
MarcCoet <marcoet@gmail.com>
2020-03-04 14:51:36 +0100

    may need to bundle in a more classic way for SSR

--- snowpack.config.js
+++ snowpack.config.js
@@ -6,32 +6,19 @@ const IS_PRODUCTION_MODE = process.env.NODE_ENV === 'production'
 
 module.exports = {
   rollup: {
-    // input: config.server.input(),
-    // output: config.server.output(),
+    // installOptions: {
+    //   externalPackage: ['svelte-routing'],
+    // },
     plugins: [
       svelte({
-        // generate: 'ssr',
-        // hydratable: true,
-        // dev: !IS_PRODUCTION_MODE,
+        generate: 'ssr',
+        hydratable: true,
+        dev: !IS_PRODUCTION_MODE,
       }),
-      // replace({
-      //   'process.browser': false,
-      //   'process.env.NODE_ENV': JSON.stringify(mode)
-      // }),
-      // svelte({
-      //   generate: 'ssr',
-      //   dev
-      // }),
-      // resolve({
-      //   dedupe: ['svelte']
-      // }),
-      // commonjs()
     ],
     // external: Object.keys(pkg.dependencies).concat(
     //   require('module').builtinModules ||
     //     Object.keys(process.binding('natives'))
     // ),
-
-    // onwarn,
   },
 }
