95564902c99acdeeb4fbb2c83a0be8824eebd5ea
MarcCoet <marcoet@gmail.com>
2020-03-13 13:57:58 +0100

    WIP: preload scripts

--- src/templates/index.svelte
+++ src/templates/index.svelte
@@ -5,13 +5,21 @@
 <SEO setViewport charset="utf-8">
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <!-- <link rel="preconnect" href="https://fonts.googleapis.com/" /> -->
-  <link rel="preload" href="/templates/index.js" as="script" crossorigin />
-  <link rel="preload" href="/templates/SEO.js" as="script" crossorigin />
   <link
     rel="preload"
     href="/web_modules/svelte/internal.js"
     as="script"
     crossorigin />
+  <link rel="preload" href="/templates/index.js" as="script" crossorigin />
+  <link rel="preload" href="/templates/SEO.js" as="script" crossorigin />
+  <link rel="preload" href="/components/Meta.js" as="script" crossorigin />
+
+  <link rel="preload" href="/styles/reset.css" as="style" crossorigin />
+  <link rel="preload" href="/styles/sizes.css" as="style" crossorigin />
+  <link rel="preload" href="/styles/typography.css" as="style" crossorigin />
+  <link rel="preload" href="/styles/colors.css" as="style" crossorigin />
+  <link rel="preload" href="/styles/layout.css" as="style" crossorigin />
+  <link rel="preload" href="/styles/main.css" as="style" crossorigin />
 </SEO>
 <!-- favicon="" -->
 
